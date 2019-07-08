592c21a6d05858ae799309bc095bc1c517091285
Marian Soban <mso@eea.sk>
2019-07-08 22:40:05 +0200

    configuration for ArdupilotMega (unfinished)

--- src/Config.h
+++ src/Config.h
@@ -3,7 +3,7 @@
  ##################################################################################################################*/
  #define CONFIG_VERSION 1003 // Changing this will reset eeprom to default values
 //########## BOARD ################################################################################################
-// #define TEENSYPLUS2 // Teensy++2 support. 
+// #define TEENSYPLUS2 // Teensy++2 support.
 #define MEGA // Arduino Mega board
 
 //########## OPTIONS ###############################################################################################
@@ -12,15 +12,15 @@
    Most Arduino have a +2.18% error at 57600 bd, Teensyduino has a -0.74% error. Booth cumulated are too much.
    Successfull com between Teensy & arduino requires 58824 bauds for Teensy.*/
 #define BAUDRATE56K 57600
-// #define BAUDRATE56K 58824 
- 
+// #define BAUDRATE56K 58824
+
  #define BARO_ALT // Use Baro for Altitude. Comment for using GPS altitude instead.
- 
+
 //Use Mag+imu for heading or GPS heading if not set ( not used for tracker only osd relay )
  #define MAGHEADING 1
-                         
-               
- #define MAGDEC -600  // Your local Magnetic Declination in radian. Get it from here: http://magnetic-declination.com/  then convert it in milliradian: http://www.wolframalpha.com/input/?i=%280%C2%B0+5%27%29+in+radians 
+
+
+ #define MAGDEC -600  // Your local Magnetic Declination in radian. Get it from here: http://magnetic-declination.com/  then convert it in milliradian: http://www.wolframalpha.com/input/?i=%280%C2%B0+5%27%29+in+radians
                      // only needed if using internal compass.
 
 //Minimum distance in meters where it will stop moving servos.
@@ -38,23 +38,23 @@
  #define BANK2  "5.8 Ghz"
  #define BANK3  "Bank 3"
  #define BANK4  "Bank 4"
- 
+
  //GS Battery alarm
  #define MIN_VOLTAGE1 10.5f // First battery alarm level. Will emit 2 short tones every 10 sec.
  #define MIN_VOLTAGE2 10.0f // Second battery alarm level. Will emit 1 short + 1 long tone every 5 sec
  #define VOLTAGE_RATIO 600   // Default multiplier for battery voltage reading * 100. This can eb adjustd later from the menu.
- 
+
  // Minimum voltage for lipo alert
 //########### GROUND OSD TELEMETRY OUTPUT #########################################################################
 // Activate osd output (comment if not needed)
- #define OSD_OUTPUT 
+ #define OSD_OUTPUT
 //OSD output baudrate ( send data as fast as possible to the osd, no need to have the same baudrate as input one. )
  #define OSD_BAUD 57600
 //########### LCD ##################################################################################################
 //LCD model
 #define LCDLCM1602 // (adress: 0x27 or 0x20) HobbyKing IIC/I2C/TWI Serial 2004 20x4, LCM1602 IIC A0 A1 A2 & YwRobot Arduino LCM1602 IIC V1
 //#define LCDGYLCD  // (adress: 0x20) Arduino-IIC-LCD GY-LCD-V1Arduino-IIC-LCD GY-LCD-V1
-//#define LCD03I2C  // (adress: 0x63 or  0xc6) LCD03 / LCD05 
+//#define LCD03I2C  // (adress: 0x63 or  0xc6) LCD03 / LCD05
 //#define GLCDEnable // Graphical LCD - Using system5x7 font so its nearly 20x4 size
 //#define OLEDLCD  // Oled 128x64 i2c LCD (address 0x3C or 0x3D)
 
@@ -68,11 +68,11 @@
 // To prevent burning servo they boot starts at neutral for all values. Adjust them directly from the menu.
 
  #define PAN_MAXPWM 1500     //max pan servo pwm value
- #define PAN_MAXANGLE 90     //Max angle clockwise (on the right) relative to PAN_MAXPWM. 
+ #define PAN_MAXANGLE 90     //Max angle clockwise (on the right) relative to PAN_MAXPWM.
  #define PAN_MINPWM 1500     //min pan servo pwm valuemin pan servo pwm value
  #define PAN_MINANGLE 90	    //Max angle counter clockwise (on the left) relative to PAN_MINPWM.
 
- #define TILT_MAXPWM 1500    //max tilt pwm value 
+ #define TILT_MAXPWM 1500    //max tilt pwm value
  #define TILT_MAXANGLE 90    //max tilt angle considering 0° is facing toward.
  #define TILT_MINPWM 1500    //min tilt pwm value
  #define TILT_MINANGLE 0     //minimum tilt angle. Considering 0 is facing toward, a -10 value would means we can tilt 10° down.
@@ -83,8 +83,8 @@
 //DON'T EDIT THIS IF YOU DON'T KNOW WHAT YOU'RE DOINGG
 //pinout for TEENSY++ 2
 #ifdef TEENSYPLUS2
-  #define PAN_SERVOPIN     26    //PWM Pin for pan servo 
-  #define TILT_SERVOPIN    25 	 //PWM Pin for tilt servo 
+  #define PAN_SERVOPIN     26    //PWM Pin for pan servo
+  #define TILT_SERVOPIN    25 	 //PWM Pin for tilt servo
   #define LEFT_BUTTON_PIN  10    //Any Digital pin
   #define RIGHT_BUTTON_PIN  9    //Any Digital pin
   #define ENTER_BUTTON_PIN  8    //Any Digital pin
@@ -96,16 +96,16 @@
 
 //pinout for Arduino Mega 1280/2560
 #ifdef MEGA
-  #define PAN_SERVOPIN     11	//PWM Pin for pan servo
-  #define TILT_SERVOPIN    12   //PWM Pin for tilt ervo
-  #define LEFT_BUTTON_PIN  32   //Any Digital pin
-  #define RIGHT_BUTTON_PIN 34   //Any Digital pin
-  #define ENTER_BUTTON_PIN 36   //Any Digital pin
+  #define PAN_SERVOPIN     11	//PWM Pin for pan servo ---> (PB5) - APM OUT PWM 2
+  #define TILT_SERVOPIN    12   //PWM Pin for tilt servo ---> (PB6) - APM OUT PWM 1
+  #define LEFT_BUTTON_PIN  32   //Any Digital pin ---> (PC5)
+  #define RIGHT_BUTTON_PIN 34   //Any Digital pin ---> (PC3)
+  #define ENTER_BUTTON_PIN 36   //Any Digital pin ---> (PC1)
   #define ADC_VOLTAGE      41   //(A5) ADC pin used for voltage reading
-  #define BUZZER_PIN        8   //(PH5) Any PWM pin ((add a 100-150 ohm resistor between buzzer & ground)
+  #define BUZZER_PIN        8   //(PH5) Any PWM pin ((add a 100-150 ohm resistor between buzzer & ground) ---> (PH5?) - APM OUT PWM 3
 #endif
 
 //################################################## DEBUG ##########################################################
-//#define DEBUG
+#define DEBUG
 
 //###############################################END OF CONFIG#######################################################
